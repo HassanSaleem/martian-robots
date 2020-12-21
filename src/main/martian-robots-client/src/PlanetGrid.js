63be4a360b180a25e8f99b08e5afb30479a2e54e
HassanSaleem <hassan100@gmail.com>
2020-12-21 14:50:47 +0000

    adjust grid positioning

--- src/main/martian-robots-client/src/PlanetGrid.js
+++ src/main/martian-robots-client/src/PlanetGrid.js
@@ -7,7 +7,7 @@ function PlanetGrid(){
     var gridRobots = useSelector(state => state.robots.gridRobots)
     console.log(coordinates)
     return (
-        <div style={{margin: "auto", backgroundColor:"#9D0000", height:((coordinates.yPosition * 100) + 70) + "px", width:((coordinates.xPosition *100) + 70) + "px"}}>
+        <div style={{postion: "absolute", margin: "auto", backgroundColor:"#9D0000", height:((coordinates.yPosition * 100) + 70) + "px", width:((coordinates.xPosition *100) + 70) + "px"}}>
             {gridRobots.map((obj) => {
                 var colour = "#FFD700"
                 var gridPosition = {
@@ -25,7 +25,7 @@ function PlanetGrid(){
                 return(
                 <div style={{height: "50px", width:"50px",position:"relative", left:gridPosition.xPosition*100 + "px", top:(coordinates.yPosition * 100 - gridPosition.yPosition*100) + "px"}}>
                     <RobotLogo style={{fill: colour}}/>
-                        <div style={{textAlign: "center", color:colour}}>{obj.gridPosition.stringFormat}</div>
+                        <div style={{textAlign: "center", fontSize: "10px", color:colour}}>{obj.gridPosition.stringFormat}</div>
                     </div>)
                 })}
         </div>
